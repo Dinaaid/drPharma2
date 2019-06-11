44ebe5dc0eee9e79fe57705133b5a9ac9f3cc81f
Dinaaid <dinaaid3@gmail.com>
2019-06-11 09:12:34 +0200

    init commit

---
+++ Pharmacy-master - Copy/src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { AuthService } from "./services/auth.service";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MedicinesComponent } from './medicines/medicines.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SigninComponent } from './signin/signin.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SearchComponent } from './search/search.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDAUT1VrYuEpw7ZqLhDMp4lBXXX1j_NYYo",
+  authDomain: "angularfire2-8ae47.firebaseapp.com",
+  databaseURL: "https://angularfire2-8ae47.firebaseio.com",
+  projectId: "angularfire2-8ae47",
+  storageBucket: "angularfire2-8ae47.appspot.com",
+  messagingSenderId: "711574640048"
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MedicinesComponent,
+    ServicesComponent,
+    HomeComponent,
+    HeaderComponent,
+    SigninComponent,
+    ContactUsComponent,
+    SearchComponent,
+    ShoppingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ScrollToModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
